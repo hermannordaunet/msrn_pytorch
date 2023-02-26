37482f28df4d23df17bb0471f9b4a857d21c95f4
hermanno <hermanno@uio.no>
2023-02-26 12:46:12 +0100

    format file

--- src/models/small_dqn.py
+++ src/models/small_dqn.py
@@ -4,7 +4,9 @@ import torch.nn.functional as F
 
 
 class small_DQN(nn.Module):
-    def __init__(self, in_channels, img_height, img_width, num_classes, memory):
+    def __init__(
+        self, in_channels, img_height, img_width, num_classes, memory, dropout_prob=0.5
+    ):
         super(small_DQN, self).__init__()
         # ---- CONVOLUTIONAL NEURAL NETWORK ----
         HIDDEN_LAYER_1_OUT = 16
@@ -15,14 +17,20 @@ class small_DQN(nn.Module):
 
         self._in_channels = in_channels
         self._img_height = img_height
-        self._img_height = img_width
+        self._img_width = img_width
         self._num_classes = num_classes
         self.memory = memory
 
+        # Layer 1 with batch norm
         self.conv1 = nn.Conv2d(
-            self._num_classes, HIDDEN_LAYER_1_OUT, kernel_size=KERNEL_SIZE, stride=STRIDE
+            self._num_classes,
+            HIDDEN_LAYER_1_OUT,
+            kernel_size=KERNEL_SIZE,
+            stride=STRIDE,
         )
         self.bn1 = nn.BatchNorm2d(HIDDEN_LAYER_1_OUT)
+
+        # Layer 2 with batch norm
         self.conv2 = nn.Conv2d(
             HIDDEN_LAYER_1_OUT,
             HIDDEN_LAYER_2_OUT,
@@ -30,6 +38,8 @@ class small_DQN(nn.Module):
             stride=STRIDE,
         )
         self.bn2 = nn.BatchNorm2d(HIDDEN_LAYER_2_OUT)
+
+        # Layer 3 with batch norm
         self.conv3 = nn.Conv2d(
             HIDDEN_LAYER_2_OUT,
             HIDDEN_LAYER_3_OUT,
@@ -38,20 +48,30 @@ class small_DQN(nn.Module):
         )
         self.bn3 = nn.BatchNorm2d(HIDDEN_LAYER_3_OUT)
 
+        # Dropout layer for generalization and overfitting
+        self.dropout = nn.Dropout(dropout_prob)
+
+        # DELETE: Softmax for making a conf score. Delete when EE is implemented
         self.softmax = nn.Sequential(
             nn.Softmax(dim=1),
         )
 
         # Number of Linear input connections depends on output of conv2d layers
-        # and therefore the input image size, so compute it.
+        # and therefore the input image size. This function computes it
         def conv2d_size_out(size, kernel_size=KERNEL_SIZE, stride=STRIDE):
             return (size - (kernel_size - 1) - 1) // stride + 1
 
-        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(self.w)))
-        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(self.h)))
-        linear_input_size = convw * convh * 32
-        nn.Dropout()
-        self.head = nn.Linear(linear_input_size, self.num_classes)
+        # Calculate the number of in features for the bootleneck linear layer (fc1). 
+        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(self._img_width)))
+        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(self._img_height)))
+        linear_input_size = convw * convh * HIDDEN_LAYER_3_OUT
+
+        # bottleneck Linear layer
+        self.fc1 = nn.Linear(linear_input_size, 128)
+
+        # Last linear for class probability distribution
+        self.fc2 = nn.Linear(128, self._num_classes)
+
 
     # Called with either one element to determine next action, or a batch
     # during optimization. Returns tensor([[left0exp,right0exp]...]).
@@ -59,7 +79,11 @@ class small_DQN(nn.Module):
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
-        x = self.head(x.view(x.size(0), -1))
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
 
-        conf = torch.max(self.softmax(x)).item()
-        return x, conf
+        # DELETE: Remove the conf
+        # conf = torch.max(self.softmax(x)).item()
+        return x #, conf
