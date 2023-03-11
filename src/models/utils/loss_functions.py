d7cff54d254a57fdd471734774b199a7f1848749
hermanno <hermanno@uio.no>
2023-03-11 12:07:16 +0100

    Tried to fix the nan problem in the loss function

--- src/models/utils/loss_functions.py
+++ src/models/utils/loss_functions.py
@@ -15,14 +15,20 @@ def loss_v2(num_ee, pred, target, conf, cost, lambda_coef=1.0):
     This loss function is the cumulative loss of loss_v1 by recursively.
     It aims to provide a more fair training.
     """
-    cum_pred = [None] * num_ee + [pred[num_ee]]
-    cum_cost = [None] * num_ee + [cost[num_ee]]
-    pred_loss = F.nll_loss(cum_pred[-1].log(), target)
+    cum_pred = [None] * num_ee + [pred[-1]]
+    cum_cost = [None] * num_ee + [cost[-1]]
+
+    # TODO: Find out why they do .log here:
+    # ASK: ^
+    # log_cum_pred = cum_pred[-1].log()
+    log_cum_pred = cum_pred[-1]
+
+    pred_loss = F.nll_loss(log_cum_pred, target)
     cum_loss = pred_loss + lambda_coef * cum_cost[-1].mean()
     for i in range(num_ee - 1, -1, -1):
         cum_pred[i] = conf[i] * pred[i] + (1 - conf[i]) * cum_pred[i + 1]
         cum_cost[i] = conf[i] * cost[i] + (1 - conf[i]) * cum_cost[i + 1]
-        pred_loss = F.nll_loss(cum_pred[i].log(), target)
+        pred_loss = F.nll_loss(cum_pred[i], target)
         cost_loss = cum_cost[i].mean()
         cum_loss += pred_loss + lambda_coef * cost_loss
 
