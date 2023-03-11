7a5951cef0dc5d7f85bc2b7f0f99cc6863a70a07
hermanno <hermanno@uio.no>
2023-03-11 15:43:17 +0100

    Commented out the plotting

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -210,20 +210,20 @@ def main():
         )
     )
 
-    # plot the training loss and accuracy
-    plt.style.use("ggplot")
-    plt.figure()
-    plt.plot(H["train_loss"], label="train_loss")
-    plt.plot(H["val_loss"], label="val_loss")
-    plt.plot(H["train_acc"], label="train_acc")
-    plt.plot(H["val_acc"], label="val_acc")
-    plt.title("Training Loss and Accuracy on Dataset")
-    plt.xlabel("Epoch #")
-    plt.ylabel("Loss/Accuracy")
-    plt.legend(loc="lower left")
-    plt.savefig(r"./reports/figures/plot.png")
-    # serialize the model to disk
-    torch.save(model, r"./models/model.pt")
+    # # plot the training loss and accuracy
+    # plt.style.use("ggplot")
+    # plt.figure()
+    # plt.plot(H["train_loss"], label="train_loss")
+    # plt.plot(H["val_loss"], label="val_loss")
+    # plt.plot(H["train_acc"], label="train_acc")
+    # plt.plot(H["val_acc"], label="val_acc")
+    # plt.title("Training Loss and Accuracy on Dataset")
+    # plt.xlabel("Epoch #")
+    # plt.ylabel("Loss/Accuracy")
+    # plt.legend(loc="lower left")
+    # plt.savefig(r"./reports/figures/plot.png")
+    # # serialize the model to disk
+    # torch.save(model, r"./models/model.pt")
 
 
 if __name__ == "__main__":
