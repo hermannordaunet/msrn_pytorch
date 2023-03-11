0994f5a802d53ba2aacc0553d039fb8377a1f366
hermanno <hermanno@uio.no>
2023-03-11 14:30:58 +0100

    Added MPS, cuda and cpu device check

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -37,7 +37,13 @@ def main():
 
     # set the device we will be using to train the model
     # device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
-    device = "cpu"
+    if torch.cuda.is_available():
+        device = "cuda"
+    elif torch.backends.mps.is_available():
+        device = "mps"
+    else:
+        device = "cpu"
+
     print(f"Device is: {device}")
 
     # load the KMNIST dataset
