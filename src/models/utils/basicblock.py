2810ef910f4050d019cb5ca0bab50212e9522558
hermanno <hermanno@uio.no>
2023-03-24 13:44:42 +0100

    added a basic block for making CNN with residual blocks

---
+++ src/models/utils/basicblock.py
@@ -0,0 +1,47 @@
+import torch.nn as nn
+
+class BasicBlock(nn.Module):
+    """Basic Block defition.
+
+    Basic 3X3 convolution blocks for use on ResNets with layers <= 34.
+    Follows improved proposed scheme in http://arxiv.org/pdf/1603.05027v2.pdf
+    TODO: Read and add this paper into my master
+    """
+
+    # TODO: Needed?
+    # expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+def conv3x3(in_planes, out_planes, stride=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(
+        in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False
+    )
