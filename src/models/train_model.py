ddcd0452c65da2ca060d4315f7881db996e31b31
hermanno <hermanno@uio.no>
2023-03-24 13:37:08 +0100

    fixed the min_max conf function

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -25,13 +25,16 @@ from small_dqn_ee import small_DQN_EE
 from utils.loss_functions import loss_v1, loss_v2
 
 
-def random_max_min_conf_from_batch(conf_list: list(), seed=None) -> tuple():
+def max_min_conf_from_dataset(conf_list: list(), seed=None) -> tuple():
     # Calculate max and min conf of each exit suring training
     # get the number of columns
-    rng = np.random.default_rng(seed)
+    # rng = np.random.default_rng(seed)
+    # random_idx = rng.integers(low=0, high=len(conf_list))
 
-    random_idx = rng.integers(low=0, high=len(conf_list))
-    tensor_of_conf = torch.cat(conf_list[random_idx], dim=1)
+    tensor_of_conf = torch.Tensor()
+    for batch_conf_list in conf_list:
+        tensor_of_conf_idx = torch.cat(batch_conf_list, dim=1).cpu()
+        tensor_of_conf = torch.cat((tensor_of_conf, tensor_of_conf_idx), dim=0)
 
     min_vals, _ = torch.min(tensor_of_conf, dim=0)
     max_vals, _ = torch.max(tensor_of_conf, dim=0)
