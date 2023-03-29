024114b5a6a737687393a39834ea007c004314b8
hermanno <hermanno@uio.no>
2023-03-29 19:44:04 +0200

    added log()

--- src/models/utils/loss_functions.py
+++ src/models/utils/loss_functions.py
@@ -57,7 +57,7 @@ def loss_v2(num_ee, pred, target, conf, cost, lambda_coef=1.0):
     for i in range(num_ee - 1, -1, -1):
         cum_pred[i] = conf[i] * pred[i] + (1 - conf[i]) * cum_pred[i + 1]
         cum_cost[i] = conf[i] * cost[i] + (1 - conf[i]) * cum_cost[i + 1]
-        pred_loss = F.nll_loss(cum_pred[i], target)
+        pred_loss = F.nll_loss(cum_pred[i].log(), target)
         cost_loss = cum_cost[i].mean()
         cum_loss += pred_loss + lambda_coef * cost_loss
 
