4d238ddd1308c7f618a29012c241de9bddcf5899
hermanno <hermanno@uio.no>
2023-05-01 14:33:18 +0200

    Changed the order or the named tuple. state, action, reward instead of state, action, next_state

--- src/models/utils/replay_memory.py
+++ src/models/utils/replay_memory.py
@@ -1,17 +1,33 @@
+import random
+
 import numpy as np
 
 from collections import namedtuple
 
 # Memory for Experience Replay
 class ReplayMemory(object):
-    def __init__(self, capacity):
+    def __init__(self, capacity, batch_size=32):
         self.capacity = capacity
-        self.memory = []
+        self.batch_size = batch_size
+        self.memory = list()
         self.position = 0
 
         self.Transition = namedtuple(
-            "Transition", ("state", "action", "next_state", "reward")
+            "Transition", ("state", "action", "reward", "next_state", "done")
         )
+
+    def add(self, *args):
+        """Saves a transition."""
+        if len(self.memory) < self.capacity:
+            self.memory.append(
+                None
+            )  # if we haven't reached full capacity, we append a new transition
+        self.memory[self.position] = self.Transition(*args)
+        self.position = (
+            self.position + 1
+        ) % self.capacity  # e.g if the capacity is 100, and our position is now 101, we don't append to
+        # position 101 (impossible), but to position 1 (its remainder), overwriting old data
+
 
     def push(self, *args):
         """Saves a transition."""
@@ -25,9 +41,13 @@ class ReplayMemory(object):
         ) % self.capacity  # e.g if the capacity is 100, and our position is now 101, we don't append to
         # position 101 (impossible), but to position 1 (its remainder), overwriting old data
 
-    def sample(self, batch_size):
-        return np.random.choice(self.memory, size=batch_size)
-        # return random.sample(self.memory, batch_size)
+    def sample(self):
+        indexes = np.random.choice(range(len(self.memory)), self.batch_size, replace=False)
+        # return np.random.choice(self.memory, size=self.batch_size)
+
+        experiences = [self.memory[i] for i in indexes]
+
+        return experiences
 
     def __len__(self):
         return len(self.memory)
