df647961aa26a90a1db8329f4a48442f6b759fb1
hermanno <hermanno@uio.no>
2023-05-05 13:56:34 +0200

    changed to seaborn and dataframe

--- visualization/visualize.py
+++ visualization/visualize.py
@@ -1,5 +1,8 @@
 import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
+
+import pandas as pd
 
 # TODO: Add some plotting functions into this.
 # Training loss and score
@@ -8,22 +11,23 @@ import matplotlib.pyplot as plt
 # save scores plot
 def plot_scores_from_list(scores: list(), labels=None, env_name="", result_dir="./"):
     scores = np.array(scores).squeeze()
+
     if scores.ndim > 1:
-        x_values = np.arange(len(scores[-1]))
+        x_values = np.arange(1, scores[-1].size + 1)
     else:
-        x_values = np.arange(len(scores))
+        x_values = np.arange(1, scores.size + 1)
 
     if labels:
         if scores.ndim > 1:
-            if len(scores) > len(labels):
+            if scores.shape[0] > len(labels):
                 print("Need provide enough labels to get legend on the plot")
                 labels = None
 
-        else:
-            labels = labels[0]
+    df = pd.DataFrame(scores.transpose(), columns=labels, index=x_values)
 
-    plt.figure()
-    plt.plot(x_values, scores.transpose(), label=labels)
+    sns.set_theme(style="darkgrid")
+    sns.lineplot(data=df)
+    sns.despine()
     plt.title(f"{env_name} agent reward")
     plt.ylabel("Reward")
     plt.xlabel("Episode #")
@@ -39,21 +43,23 @@ def plot_scores_from_list(scores: list(), labels=None, env_name="", result_dir="
 # save loss plot
 def plot_loss_from_list(losses: list(), labels=None, env_name="", result_dir="./"):
     losses = np.array(losses).squeeze()
+
     if losses.ndim > 1:
-        x_values = np.arange(len(losses[-1]))
+        x_values = np.arange(1, losses[-1].size + 1)
     else:
-        x_values = np.arange(len(losses))
+        x_values = np.arange(1, losses.size + 1)
 
     if labels:
         if losses.ndim > 1:
-            if len(losses) > len(labels):
+            if losses.shape[0] > len(labels):
                 print("Need provide enough labels to get legend on the plot")
                 labels = None
-        else:
-            labels = labels[0]
 
-    plt.figure()
-    plt.plot(x_values, losses.transpose(), label=labels)
+    df = pd.DataFrame(losses.transpose(), columns=labels, index=x_values)
+
+    sns.set_theme(style="darkgrid")
+    sns.lineplot(data=df)
+    sns.despine()
     plt.title(f"{env_name} loss")
     plt.ylabel("Loss")
     plt.xlabel("Episode #")
