81f60d85905ce6f74e901c47a83a1c6bd9257ab4
hermanno <hermanno@uio.no>
2023-08-16 12:22:14 +0200

    added new classifier, changed name of old

--- src/models/utils/classifier.py
+++ src/models/utils/classifier.py
@@ -1,6 +1,12 @@
 import torch.nn as nn
 
 
-def simple_classifier(num_classes, in_size):
+def classifier_linear_softmax(num_classes, in_size):
     """get classifier as nn.Sequential"""
     return nn.Sequential(nn.Linear(in_size, num_classes), nn.Softmax(dim=1))
+
+
+def classifier_linear(num_classes, in_size):
+    """get classifier as nn.Sequential"""
+    return nn.Sequential(nn.Linear(in_size, num_classes))
+
