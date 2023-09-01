b48014bfa7650d1e117fa19fa36d1c9907062ebf
hermanno <hermanno@uio.no>
2023-09-01 12:03:23 +0200

    fixing printout etc

--- src/models/evaluate_unity.py
+++ src/models/evaluate_unity.py
@@ -1,5 +1,7 @@
 import torch
 
+import numpy as np
+
 # unity imports
 from mlagents_envs.base_env import ActionTuple
 
@@ -12,8 +14,24 @@ from mlagents_envs.exception import (
 # Local imports
 from src.models.utils.data_utils import get_grid_based_perception
 
+def get_scores_of_all_agents(eval_agents: dict(), flatten=False):
+    if flatten:
+        # Flatten the scores into a 1D list
+        scores = [eval_agents[team][j]['episode_score'] for team in ['team0', 'team1', 'team2'] for j in range(4)]
+    else:
+        # Initialize a 3x4 list with zeros
+        scores = [[0] * 4 for _ in range(3)]
 
-def evaluate_trained_model(env, agent, config, verbose=False):
+        # Iterate through teams and episodes to fill the score_matrix
+        for i, team in enumerate(['team0', 'team1', 'team2']):
+            for j in range(4):
+                scores[i][j] = eval_agents[team][j]['episode_score']
+
+    return scores
+
+
+
+def evaluate_trained_model(env, agent, config, current_episode, verbose=False):
     num_eval_episodes = config["eval"]["episodes"]
 
     team_name_list = list(env.behavior_specs.keys())
@@ -41,6 +59,8 @@ def evaluate_trained_model(env, agent, config, verbose=False):
             env.reset()
 
             for _, team in enumerate(team_name_list):
+                eval_agents[team] = {}
+
                 decision_steps, _ = env.get_steps(team)
                 agents_need_action = decision_steps.agent_id
                 for agent_id in agents_need_action:
@@ -48,10 +68,9 @@ def evaluate_trained_model(env, agent, config, verbose=False):
                     state = get_grid_based_perception(agent_obs)
                     state_batch_tensor[agent_id, ...] = state
 
-                    eval_agents[team] = {
-                        "agent_id": agent_id,
+                    eval_agents[team][agent_id] = {
                         "episode_score": 0,
-                    }
+                    } 
 
             episode_done = False
 
@@ -83,12 +102,16 @@ def evaluate_trained_model(env, agent, config, verbose=False):
                         state_batch_tensor[agent_id, ...] = state
 
                         agent_reward = decision_steps[agent_id].reward
-                        eval_agents[team]["episode_score"] += agent_reward
+                        eval_agents[team][agent_id]["episode_score"] += agent_reward
 
                     terminated_agent_ids = terminal_steps.agent_id
                     done = True if len(terminated_agent_ids) > 0 else False
                     episode_done = done
-
+            
+            eval_scores_all_agents = get_scores_of_all_agents(eval_agents, flatten=True)
+            mean_score = np.mean(eval_scores_all_agents)
+            print(f"[INFO] Mean performance on policy net after {current_episode} episodes: {mean_score}")
+        
     except (
         KeyboardInterrupt,
         UnityCommunicationException,
@@ -101,12 +124,12 @@ def evaluate_trained_model(env, agent, config, verbose=False):
         print("Evaluation was interrupted.")
         print("-" * 100)
         env.close()
-    finally:
-        if eval_agents:
-            eval_scores_all_agents = [
-                team_info["episode_score"] for team_info in eval_agents.values()
-            ]
-        else:
-            eval_scores_all_agents = None
+    # finally:
+        # if eval_agents:
+        #     eval_scores_all_agents = [
+        #         team_info["episode_score"] for team_info in eval_agents.values()
+        #     ]
+        # else:
+        #     eval_scores_all_agents = None
 
-        return eval_scores_all_agents
+        # return eval_scores_all_agents
