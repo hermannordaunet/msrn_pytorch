ce8679c34e720caa54d8b5cdb386905759195805
hermanno <hermanno@uio.no>
2023-09-01 12:22:12 +0200

    removed nn.Sequential from linear classifier

--- src/models/utils/classifier.py
+++ src/models/utils/classifier.py
@@ -8,5 +8,5 @@ def classifier_linear_softmax(in_size, num_classes):
 
 def classifier_linear(in_size, num_classes):
     """get classifier as nn.Sequential"""
-    return nn.Sequential(nn.Linear(in_size, num_classes))
+    return nn.Linear(in_size, num_classes)
 
