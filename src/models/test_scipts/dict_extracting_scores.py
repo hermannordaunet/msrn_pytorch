5bd121a345345c854561235c4b5992f53760ae52
hermanno <hermanno@uio.no>
2023-09-10 15:59:58 +0200

    test scripts

--- src/models/test_scipts/dict_extracting_scores.py
+++ src/models/test_scipts/dict_extracting_scores.py
@@ -5,30 +5,25 @@ eval_agents = {
         7: {"episode_score": 8},
         11: {"episode_score": 9},
     },
-    "GridFoodCollector?team=0": {
-        1: {"episode_score": 2},
-        5: {"episode_score": 3},
-        10: {"episode_score": 4},
-    },
-    "GridFoodCollector?team=3": {2: {"episode_score": 1}, 6: {"episode_score": 7}},
-    "GridFoodCollector?team=2": {
-        3: {"episode_score": 6},
-        4: {"episode_score": 0},
-        8: {"episode_score": 10},
-        12: {"episode_score": 11},
-        19: {"episode_score": 12},
-    },
 }
 
 
-def extract_scores(eval_agents, flatten=False):
+def extract_scores_for_all_agents(
+    eval_agents: dict, all_agents_active=True, flatten=False
+):
     # Extract the team keys from the eval_agents
     team_keys = list(eval_agents.keys())
 
     # Extract all episode keys from all teams' dictionaries
     agent_keys = list()
     for team_key in team_keys:
-        agent_keys.append(tuple(eval_agents[team_key].keys()))
+        agents_in_team = list(eval_agents[team_key].keys())
+
+        if all_agents_active:
+            agent_keys.append(tuple(agents_in_team))
+
+        else:
+            agent_keys.append([agents_in_team[-1]])
 
     if flatten:
         # Flatten the scores into a 1D list
@@ -39,12 +34,11 @@ def extract_scores(eval_agents, flatten=False):
     else:
         # Initialize a 2D list with zeros based on the number of teams and episodes
         num_teams = len(team_keys)
-
         scores = [None] * num_teams
 
         # Iterate through teams and episodes to fill the scores
         for i, team_key in enumerate(team_keys):
-            scores[i] = [0]*len(agent_keys[i])
+            scores[i] = [0] * len(agent_keys[i])
             for j, episode_key in enumerate(agent_keys[i]):
                 if episode_key in eval_agents[team_key]:
                     scores[i][j] = eval_agents[team_key][episode_key]["episode_score"]
@@ -52,11 +46,13 @@ def extract_scores(eval_agents, flatten=False):
     return scores
 
 
-flat_scores = extract_scores(eval_agents, flatten=True)
+flat_scores = extract_scores_for_all_agents(
+    eval_agents, all_agents_active=False, flatten=True
+)
 print("1D List:")
 print(flat_scores)
 
-score_matrix = extract_scores(eval_agents)
+score_matrix = extract_scores_for_all_agents(eval_agents)
 print("2D List:")
 for row in score_matrix:
     print(row)
--- src/models/test_scipts/test_resnet_vs_eenet.py
+++ src/models/test_scipts/test_resnet_vs_eenet.py
@@ -4,70 +4,21 @@ import torch.nn.functional as F
 
 from torchvision import models
 
+import numpy as np
+import random
+
 # Local import
 from src.models.ee_cnn_residual import EE_CNN_Residual
+from src.models.resnet_original import ResNet
 
 
-class DQN_ResNet(nn.Module):
-    def __init__(
-        self, nn_inputs, h, w, outputs, memory, weight_init="kaiminghe"
-    ) -> None:
-        super(DQN_ResNet, self).__init__()
-
-        self.nn_inputs = nn_inputs
-        self.h = h
-        self.w = w
-        self.outputs = outputs
-        self.memory = memory
-        self.weight_init = weight_init
-
-        self.softmax = nn.Sequential(
-            nn.Softmax(dim=1),
-        )
-
-        self.num_classes = outputs
-
-        pretrained_resnet = True
-        pretrained_net = models.resnet18(pretrained=pretrained_resnet)
-
-        print(f"[INFO] Pretrained ResNet18 loaded: {pretrained_resnet}")
-
-        print(pretrained_net)
-        original_layer = pretrained_net.conv1
-        current_weights = original_layer.weight.clone()
-
-        pretrained_net.conv1 = nn.Conv2d(
-            self.nn_inputs,
-            out_channels=original_layer.out_channels,
-            kernel_size=original_layer.kernel_size,
-            stride=original_layer.stride,
-            dilation=original_layer.dilation,
-            bias=original_layer.bias,
-        )
-
-        new_first_layer = pretrained_net.conv1.weight.clone()
-
-        # TODO: Change the first layer of resnet when Unity is involved
-        new_first_layer[:, :3] = current_weights
-
-        if self.weight_init == "kaiminghe":
-            new_first_layer[:, 3] = nn.init.kaiming_normal_(new_first_layer[:, 3])
-            new_first_layer[:, 4] = nn.init.kaiming_normal_(new_first_layer[:, 4])
-        else:
-            new_first_layer[:, 3] = nn.init.xavier_uniform_(new_first_layer[:, 3])
-            new_first_layer[:, 4] = nn.init.xavier_uniform_(new_first_layer[:, 4])
-
-        pretrained_net.conv1.weight = nn.Parameter(new_first_layer)
-
-        num_ftrs = pretrained_net.fc.in_features
-        pretrained_net.fc = nn.Linear(num_ftrs, self.outputs)
-
-        self.net = pretrained_net
-
-    def forward(self, inputs):
-        x = self.net(inputs)
-        conf = torch.max(self.softmax(x)).item()
-        return x, conf
+def set_seed(seed: int = 42) -> None:
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
 
 def main():
@@ -80,12 +31,13 @@ def main():
         repetitions=[2, 2, 2, 2],
     )
 
-    resnet_original = DQN_ResNet(
-        5,
-        280,
-        280,
-        3,
-        None,
+    set_seed(1027)
+    resnet_original = ResNet(
+        input_shape=(5, 280, 280),
+        init_planes=64,
+        planes=[64, 128, 256, 512],
+        num_classes=3,
+        repetitions=[2, 2, 2, 2],
     )
 
     image = torch.rand((1, 5, 280, 280))
