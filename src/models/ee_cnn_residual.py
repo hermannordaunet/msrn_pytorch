d2282cba22f6421ef85a61ccc38d270630d14931
hermanno <hermanno@uio.no>
2023-09-10 15:56:22 +0200

    trying to get the same parameter init as resnet

--- src/models/ee_cnn_residual.py
+++ src/models/ee_cnn_residual.py
@@ -153,14 +153,11 @@ class EE_CNN_Residual(nn.Module):
         # self.dropout = nn.Dropout(dropout_prob)
 
         in_size = planes * block.expansion
+        # self.classifier = classifier_linear(in_size, self.num_classes)
         self.classifier = classifier_linear(in_size, self.num_classes)
         self.confidence = confidence_linear_sigmoid(in_size)
 
         self.stages.append(nn.Sequential(*self.layers))
-
-        # Needs to be here to get the correct cost
-        self.complexity.append((total_flops, total_params))
-
         if initalize_parameters:
             self.parameter_initializer()
 
