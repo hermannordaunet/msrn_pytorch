c5578710aaf36d9eef033d7a98bc8d2697e07a45
hermanno <hermanno@uio.no>
2023-09-10 15:53:45 +0200

    made it similar to the original

--- src/models/utils/basicblock.py
+++ src/models/utils/basicblock.py
@@ -1,10 +1,16 @@
 import torch.nn as nn
 from torch import Tensor
 
+
 def conv3x3(in_planes, out_planes, stride=1):
     """3x3 convolution with padding"""
     return nn.Conv2d(
-        in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False
+        in_planes,
+        out_planes,
+        kernel_size=3,
+        stride=stride,
+        padding=1,
+        bias=False,
     )
 
 
