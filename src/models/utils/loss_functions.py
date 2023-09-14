c421358efcd28e8643bb01332742da44712bb8cf
hermanno <hermanno@uio.no>
2023-09-14 14:33:06 +0200

    new loss

--- src/models/utils/loss_functions.py
+++ src/models/utils/loss_functions.py
@@ -126,3 +126,38 @@ def loss_v5(pred, target, num_ee=0):
     return pred_loss, 0, 0
 
 
+def loss_v6(pred, target, conf, cost, num_ee=0, lambda_coef=1.0):
+    """loss version 3
+
+    * pred:           prediction result of each exit point.
+    * target:         target prediction values.
+    * conf:           confidence value of each exit point.
+    * cost:           cost rate of the each exit point.
+    * num_ee:         number of early exit blocks
+    * lambda_coef:
+
+    This loss function is the cumulative loss of loss_v1 by recursively.
+    It aims to provide a more fair training.
+
+    First loss written for DQN training loop
+    """
+    cumulative_pred = [None] * num_ee + [pred[num_ee]]
+    cumulative_cost = [None] * num_ee + [cost[num_ee]]
+
+    # TODO: Find out why they do .log here:
+    # ASK: ^
+    # log_cumulative_pred = cumulative_pred[-1]
+    # log_cumulative_pred = cumulative_pred[-1]
+
+    # pred_loss = F.nll_loss(cumulative_pred[-1], target)
+    pred_loss = F.mse_loss(cumulative_pred[-1], target)
+    cumulative_loss = pred_loss + lambda_coef * cumulative_cost[-1]  # .mean()
+    for i in range(num_ee - 1, -1, -1):
+        cumulative_pred[i] = conf[i] * pred[i] + (1 - conf[i]) * cumulative_pred[i + 1]
+        cumulative_cost[i] = conf[i] * cost[i] + (1 - conf[i]) * cumulative_cost[i + 1]
+        # pred_loss = F.nll_loss(cumulative_pred[i].log(), target)
+        pred_loss = F.mse_loss(cumulative_pred[i], target)
+        cost_loss = cumulative_cost[i].mean()
+        cumulative_loss += pred_loss + lambda_coef * cost_loss
+
+    return cumulative_loss, 0, 0
