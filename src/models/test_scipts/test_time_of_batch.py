94169bff5074bfeed9ed0f924c840d02a9308320
hermanno <hermanno@uio.no>
2023-10-08 22:09:59 +0200

    making it work on cuda

--- src/models/test_scipts/test_time_of_batch.py
+++ src/models/test_scipts/test_time_of_batch.py
@@ -22,31 +22,33 @@ model = EE_CNN_Residual(
 # model = models.resnet18(pretrained=True)
 model.eval()
 
-for_loop_times = list()
-batch_times = list()
-
+batch_test_sizes = [64, 128, 256, 512]
 num_tests = 20
-for i in range(num_tests):
-    print(f"{i}/{num_tests}")
-    images = torch.rand((512, 3, 280, 280)).to(device)
+for batch_size in batch_test_sizes:
+    for_loop_times = list()
+    batch_times = list()
+    print(f"Batch size: {batch_size}")
+    for i in range(num_tests):
+        print(f"{i}/{num_tests}")
+        images = torch.rand((batch_size, 3, 280, 280)).to(device)
 
-    st_for_loop = time.time()
-    with torch.no_grad():
-        for idx in range(images.shape[0]):
-            pred = model(images[idx, ...].unsqueeze(0))
+        st_for_loop = time.time()
+        with torch.no_grad():
+            for idx in range(images.shape[0]):
+                pred = model(images[idx, ...].unsqueeze(0))
 
-    et_for_loop = time.time()
-    elapsed_time_for_loop = et_for_loop - st_for_loop
-    for_loop_times.append(elapsed_time_for_loop)
+        et_for_loop = time.time()
+        elapsed_time_for_loop = et_for_loop - st_for_loop
+        for_loop_times.append(elapsed_time_for_loop)
 
-    st_batch = time.time()
-    with torch.no_grad():
-        pred = model(images)
+        st_batch = time.time()
+        with torch.no_grad():
+            pred = model(images)
 
-    et_batch = time.time()
-    elapsed_time_batch = et_batch - st_batch
-    batch_times.append(elapsed_time_batch)
+        et_batch = time.time()
+        elapsed_time_batch = et_batch - st_batch
+        batch_times.append(elapsed_time_batch)
 
 
-print(f"Mean time for-loop: {np.mean(for_loop_times)} seconds")
-print(f"Mean time batch: {np.mean(batch_times)} seconds")
+    print(f"Mean time for-loop {batch_size}: {np.mean(for_loop_times):5f} seconds")
+    print(f"Mean time batch {batch_size}: {np.mean(batch_times):5f} seconds")
